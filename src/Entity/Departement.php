b703c7e772639392f0519c99f19cdf512d9e755b
flo <pegeot.florian@gmail.com>
2021-05-26 10:17:16 +0200

    ajout des controllers + entitys
    ajout de la page home, région et département
    ajout des entitys departement , objet cult et region
    faire un migration pour la BDD

---
+++ src/Entity/Departement.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DepartementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DepartementRepository::class)
+ */
+class Departement
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=101)
+     */
+    private $CP;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCP(): ?string
+    {
+        return $this->CP;
+    }
+
+    public function setCP(string $CP): self
+    {
+        $this->CP = $CP;
+
+        return $this;
+    }
+}
